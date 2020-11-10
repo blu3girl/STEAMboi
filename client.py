f6bcc8db210bfc3b2dfa130aed6d543eb741eb08
Carolyn Nguyen <carolyn.nguyen1@tamu.edu>
2020-11-09 21:47:52 -0600

    changed client

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # client.py
 import os
 import random
+from discord import Embed
 from dotenv import load_dotenv
 from discord.ext import commands
 from bot import getInfo
@@ -15,11 +16,21 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='price', help="Search for price of game based on URL or keyword search.")
-async def price(ctx, query):    
-    response = getInfo(query)
-    if response == None:
-        response = f"Sorry, there are no results for '{query}'. Please try again.'"
-    print(response)
-    await ctx.send(response)
+async def price(ctx, query):
+    msg = getInfo(query)
+    if msg == None:
+        await ctx.send(f"Sorry, there are no results for '{query}'. Please try again.")
+        return 0
+
+    response = Embed(title="Game Name", description=msg['url'], color=0x00ff00)
+    for key, val in msg:
+        if key == 'dlc':
+            response.add_field(name="DLCs", value="", inline=False)
+    response.add_field(name="Field1", value="hi", inline=False)
+    response.add_field(name="Field2", value="hi2", inline=False)
+  
+    msg = getInfo(query)
+
+    await ctx.send(embed=response)
 
 bot.run(TOKEN)
